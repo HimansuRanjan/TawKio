71975ae672bf73e83969687ce6f456925b3e2bfb
Himansu Ranjan Patra <mzzhimansu@gmail.com>
2025-09-05 14:11:46 +0530

    Cors URL code updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,8 +12,10 @@ import likeRoutes from "./routes/likeRoutes";
 
 dotenv.config();
 
+const app_url = process.env.APP_URL;
+
 const app = express();
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: [`${app_url}`], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
